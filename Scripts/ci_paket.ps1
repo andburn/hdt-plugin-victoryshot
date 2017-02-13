b978db170999b25547271b8ec11588e19d0a0d7b
Andrew Burnett <andrew@andburn.info>
2017-02-13 21:12:39 +0000

    Fix paket boostrapper gitignore entry

--- Scripts/ci_paket.ps1
+++ Scripts/ci_paket.ps1
@@ -1,2 +1,3 @@
 & "$env:APPVEYOR_BUILD_FOLDER\.paket\paket.bootstrapper.exe"
-& "$env:APPVEYOR_BUILD_FOLDER\.paket\paket.exe" install
+& "$env:APPVEYOR_BUILD_FOLDER\.paket\paket.exe" install -v
+git checkout -- VictoryShot\FodyWeavers.xml
