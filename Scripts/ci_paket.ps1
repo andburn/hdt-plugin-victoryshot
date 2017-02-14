069a9f8ba699476742bc3929eb4d19d93ddccc93
Andrew Burnett <andrew@andburn.info>
2017-02-14 12:03:32 +0000

    Fix Appveyor artifacts to only pack single dll

--- Scripts/ci_paket.ps1
+++ Scripts/ci_paket.ps1
@@ -1,3 +1,3 @@
 & "$env:APPVEYOR_BUILD_FOLDER\.paket\paket.bootstrapper.exe"
-& "$env:APPVEYOR_BUILD_FOLDER\.paket\paket.exe" install -v
+& "$env:APPVEYOR_BUILD_FOLDER\.paket\paket.exe" install
 git checkout -- VictoryShot\FodyWeavers.xml
